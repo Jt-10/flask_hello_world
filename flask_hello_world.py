e4fa657597805883774b77ae0ca5ce0a70b3aef2
Jon Thorpe <jonthrp@yahoo.com>
2017-02-01 11:48:15 -0800

    Added template

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -1,31 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from os import environ
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/hello")
-def say_hi():
-    return "Hello World!"
+def hello_world():
+    return render_template('template.html',
+                  my_string="Hello World!")
 
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
+    return render_template('template.html',
+                  # To remove python string formatting, this needs another template?
+                  my_string="Hello {}!".format(name).title())
 
 @app.route("/jedi/<first>/<last>")
 def jedi_name(first, last):
-    return "Your Jedi name is '{}{}!'".format(last[:3].title(), first[:2])
+    return render_template('template.html',
+                  # To remove python string formatting, this needs another template?
+                  my_string="Your Jedi name is '{}{}!'".format(last[:3].title(), first[:2]))
 
 if __name__=="__main__":
-    app.run(host=os.environ.get('IP', ''),
-            port=os.environ.get('PORT', 8080))
+    app.run(debug=True, host="0.0.0.0", port=8080)
