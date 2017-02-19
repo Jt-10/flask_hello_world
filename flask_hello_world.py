6fdcfd10a578c4b72a225e1f649fc0c78ec14bca
Jon Thorpe <jonthrp@yahoo.com>
2017-02-18 16:43:55 -0800

    Added a few changes to app.routes

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -10,6 +10,20 @@ def hello_world():
     return render_template('template.html',
                   my_string="Hello World!")
 
+@app.route("/kitty/<name>")
+def hello_kitty(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a random picture.  Hmmm...
+        </p>
+        <img src="http://lorempixel.com/400/200">
+        """
+    return html.format(name.title())
+
+
 @app.route("/hello/<name>")
 def hello_person(name):
     return render_template('template.html',
@@ -21,6 +35,7 @@ def jedi_name(first, last):
     return render_template('template.html',
                   # To remove python string formatting, this needs another template?
                   my_string="Your Jedi name is '{}{}!'".format(last[:3].title(), first[:2]))
+
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
